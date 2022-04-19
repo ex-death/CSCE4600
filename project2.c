b282d9d5df470fd43e5490367d46bfed8f03c837
bethebullet <98484342+bethebullet@users.noreply.github.com>
2022-04-19 10:50:58 -0500

    Update project2.c

--- project2.c
+++ project2.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
 
+
+void readInputs()
+{
+  // needs to read the text example file found in Project-2-input-example.txt
+
+  // Ignore % symbol as those are comments
+  
+  // take in num_processes, and num_resources
+  
+  // take in amount of units in each resource
+  
+  // take in M, the adjacency array
+}
+
+
+
+int main()
+{
+  // call readInputs
+  
+  // use reduction method to check for deadlock
+  
+  // output message
+  return 0;
+}
