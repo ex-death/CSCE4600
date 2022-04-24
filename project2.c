322f09825be1e6e68d2870d15148e836b86c6737
bethebullet <pokemonmasterbret@gmail.com>
2022-04-24 17:45:15 -0500

    Added project2.cpp

--- project2.c
+++ project2.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
-
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
 
 void readInputs()
 {
@@ -12,6 +17,95 @@ void readInputs()
   // take in amount of units in each resource
   
   // take in M, the adjacency array
+
+ifstream inFileStrm(inFileName);
+
+  int numProcs = -1, numRcrs = -1; //-1 is impossible real val
+
+  string line;
+  //LOOP THRU lines OF inFile
+  while(getline(inFileStrm, line)){
+    if((line.empty())||(line[0] == '\r')) continue; //skip blank lines
+    if(line[0] == '%') continue; //skip comment lines
+
+    if(line.find("num_processes=") != string::npos){
+      string parsedNum = line.substr(line.find("=")+1);
+      //cerr << "num_processes: " << parsedNum << endl;
+      numProcs = stoi(parsedNum);
+      continue;
+    }
+
+    if(line.find("num_resources=") != string::npos){
+      string parsedNum = line.substr(line.find("=")+1);
+      //cerr << "num_resources: " << parsedNum << endl;
+      numRcrs = stoi(parsedNum);
+      continue;
+    }
+
+    if(totalRcrs.empty()){
+      size_t numPos = 0;
+      //LOOP THRU avail resources line
+      while(numPos < line.length()){
+        size_t commaPos = line.find(",", numPos);
+        if(commaPos == string::npos) commaPos = line.length();
+        size_t numLen = commaPos - numPos;
+
+        string numStr = line.substr(numPos, numLen);
+        //cerr << "totalRcrs_num: " << numStr << endl;
+        totalRcrs.push_back(stoi(numStr));
+
+        numPos = commaPos + 1;
+      }//END LOOP THRU avail resources line
+
+      continue;
+    }//end if totalRcrs.empty()
+    
+    //if here, must be at matrix
+    if(procRqsts.size() < numProcs){//then this line represents a process
+      vector<int> newProc;
+      int rcrsIndex = numRcrs;
+
+      size_t numEndPos = line.length()-1;
+      //LOOP THRU resources
+      while(rcrsIndex != 0){
+        size_t commaPos = line.rfind(",", numEndPos);
+        size_t numLen = numEndPos - commaPos;
+
+        string numStr = line.substr(commaPos + 1, numLen);
+
+        //cerr << "rcrs_num: " << numStr << endl;
+        newProc.insert(newProc.begin(), stoi(numStr));//push_front
+
+        numEndPos = commaPos - 1;
+        --rcrsIndex;
+      }//END LOOP THRU resources
+
+      procRqsts.emplace(procRqsts.size(), newProc); //add row
+
+      continue;
+    }//if procRqsts.size() < numProcs
+
+    //if here, then the current line MUST represent a resource
+    vector<int> newRcrs;
+    int procIndex = 0;
+
+    size_t numPos = 0;
+    //LOOP THRU avail resources line
+    while(procIndex != numProcs){
+      size_t commaPos = line.find(",", numPos);
+      size_t numLen = commaPos - numPos;
+
+      string numStr = line.substr(numPos, numLen);
+      //cerr << "proc_num: " << numStr << endl;
+      newRcrs.push_back(stoi(numStr));
+
+      numPos = commaPos + 1;
+      ++procIndex;
+    }//END LOOP THRU avail resources line
+
+    rcrsAlloc.push_back(newRcrs);//add row
+
+
 }
 
 
@@ -24,4 +118,5 @@ int main()
   
   // output message
   return 0;
+
 }
---
+++ project2.cpp
@@ -0,0 +1,122 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+void readFile(string file, vector<int>& resourceTotal, map<int, vector<int>>& procRqsts, vector<vector<int>>& rcrsAlloc)
+{
+  ifstream fin(file);
+
+  int processCount;
+  int resourceCount; 
+
+  string line;
+  string stringNum;
+
+  int position;
+  int commaPos;
+  int numLen; 
+  int numEndPos;
+
+  while(getline(fin, line))
+  {
+    if(line.empty()) continue; //skip blank lines
+    if(line[0] == '\r') continue; //skip blank lines
+    if(line[0] == '%') continue; //skip comment lines
+
+    if(line.find("num_processes=") != string::npos)
+    {
+      stringNum = line.substr(line.find("=")+1);
+      processCount = stoi(stringNum);
+      continue;
+    }
+
+    if(line.find("num_resources=") != string::npos)
+    {
+      stringNum = line.substr(line.find("=")+1);
+      resourceCount = stoi(stringNum);
+      continue;
+    }
+
+    if(resourceTotal.empty())
+    {
+        position = 0;
+      
+        while(position < line.length()){
+        commaPos = line.find(",", position);
+        if(commaPos == string::npos) commaPos = line.length();
+        numLen = commaPos - position;
+
+        stringNum = line.substr(position, numLen);
+        resourceTotal.push_back(stoi(stringNum));
+
+        position = commaPos + 1;
+      }
+
+      continue;
+    } //end if resourceTotal.empty()
+    
+    //if here, must be at matrix
+    if(procRqsts.size() < processCount) //then this line represents a process
+    { 
+      vector<int> newProc;
+      int resourceIndex = resourceCount;
+
+      numEndPos = line.length()-1;
+      
+      while(resourceIndex != 0)
+      {
+        commaPos = line.rfind(",", numEndPos);
+        numLen = numEndPos - commaPos;
+
+        stringNum = line.substr(commaPos + 1, numLen);
+
+        newProc.insert(newProc.begin(), stoi(stringNum));//push_front
+
+        numEndPos = commaPos - 1;
+        --resourceIndex;
+      }
+
+      procRqsts.emplace(procRqsts.size(), newProc); //add row
+
+      continue;
+    } //if procRqsts.size() < numProcs
+
+    //if here, then the current line MUST represent a resource
+    vector<int> newRcrs;
+    int procIndex = 0;
+
+    position = 0;
+    
+    while(procIndex != processCount){
+      commaPos = line.find(",", position);
+      numLen = commaPos - position;
+
+      stringNum = line.substr(position, numLen);
+      
+      newRcrs.push_back(stoi(stringNum));
+
+      position = commaPos + 1;
+      ++procIndex;
+    }
+
+    rcrsAlloc.push_back(newRcrs);//add row
+
+  }
+}
+
+int main(int numArgs, char* argLst[]){
+  
+
+  string file = argLst[1];
+
+  vector<int> totalRcrs, availRcrs;
+  vector<vector<int>> rcrsAlloc;
+  map<int, vector<int>> procRqsts;
+
+  readFile(file, totalRcrs, procRqsts, rcrsAlloc);
+  
+}
